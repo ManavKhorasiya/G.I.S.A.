6850745d1c12b9f8e996e290a2f2bd4b424300ef
ManavKhorasiya <manavkhorasiya@gmail.com>
2020-07-21 01:07:04 +0530

    Heroku Initial Commit

---
+++ gisa/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Image
+from .models import segment
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields =  '__all__'
+
+# class segmentForm(forms.ModelForm):
+#     class Meta:
+#         model = segment
+#         fields = '__all__'
+class TempForm(forms.Form):
+    select_choices = [
+        ('yes','Yes'),
+        ('no','No')
+    ]
+    predictIt = forms.CharField(label = "Click button to predict", widget = forms.RadioSelect(choices = select_choices))
+
+class segmentForm(forms.ModelForm):
+    class Meta:
+        model = segment
+        fields = '__all__'
